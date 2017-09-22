64812e991140e53242b9d7241916163c7afc445d
Pugabyte <pugabear@bn-mc.net>
2017-09-22 01:20:07 -0500

    Reformat

--- src/main/java/me/pugabyte/jayce/Utils/Aliases.java
+++ src/main/java/me/pugabyte/jayce/Utils/Aliases.java
@@ -8,13 +8,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Aliases {
-	public final HashMap<String, String> aliases = new HashMap<>();
+    public final HashMap<String, String> aliases = new HashMap<>();
 
-	public Aliases() throws IOException {
-		List<String> config = Files.readAllLines(Paths.get("Jayce" + FileSystems.getDefault().getSeparator() + "aliases.txt"));
-		for (String line : config) {
-			String[] setting = line.split(" ");
-			aliases.put(setting[0], setting[1]);
-		}
-	}
+    public Aliases() throws IOException {
+        List<String> config = Files.readAllLines(Paths.get("Jayce" + FileSystems.getDefault().getSeparator() + "aliases.txt"));
+        for (String line : config) {
+            String[] setting = line.split(" ");
+            aliases.put(setting[0], setting[1]);
+        }
+    }
 }
