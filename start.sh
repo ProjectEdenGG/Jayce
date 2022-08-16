e9d46b29a6e85c98cad24a59938df03b956dc410
GriffinCodes <griffin@projecteden.gg>
2022-08-15 21:39:20 -0500

    Add start.sh

---
+++ start.sh
@@ -0,0 +1,9 @@
+java -jar \
+--add-opens java.base/java.net=ALL-UNNAMED \
+--add-opens java.base/java.util=ALL-UNNAMED \
+--add-opens java.base/java.lang=ALL-UNNAMED \
+--add-opens java.base/java.time=ALL-UNNAMED \
+--add-opens java.base/java.lang.ref=ALL-UNNAMED \
+--add-opens java.base/java.lang.invoke=ALL-UNNAMED \
+--add-opens java.base/java.lang.reflect=ALL-UNNAMED \
+Jayce.jar
